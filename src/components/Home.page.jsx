dc1c628c9d63bb8e786c64bc918cb340b29e31e7
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2022-08-10 05:01:20 -0400

    feat: improve link class names

--- src/components/Home.page.jsx
+++ src/components/Home.page.jsx
@@ -1,26 +1,20 @@
 import { Link } from "react-router-dom"
 
+const linkClassNames =
+  "text-blue-500 underline text-center rounded-md hover:bg-indigo-400 hover:text-white transition py-1 px-2"
+
 export const HomePage = () => {
   return (
     <div className="Home h-screen flex flex-col justify-center items-center p-2 gap-2">
       <div className="links bg-white shadow-lg flex flex-col gap-4 rounded-lg p-4">
         <h1 className="text-2xl text-center font-bold">Welcome!</h1>
-        <Link
-          className="text-blue-500 underline text-center rounded-md hover:bg-indigo-400 hover:text-white transition py-1 px-2"
-          to="/login"
-        >
+        <Link className={linkClassNames} to="/login">
           Login
         </Link>
-        <Link
-          className="text-blue-500 underline text-center rounded-md hover:bg-indigo-400 hover:text-white transition py-1 px-2"
-          to="/signup"
-        >
+        <Link className={linkClassNames} to="/signup">
           Signup
         </Link>
-        <Link
-          className="text-blue-500 underline text-center rounded-md hover:bg-indigo-400 hover:text-white transition py-1 px-2"
-          to="/settings"
-        >
+        <Link className={linkClassNames} to="/settings">
           Settings
         </Link>
       </div>
